a489c00f5f53e8684a312b0510cc2bc9a3578599
Casey-Litmer <litmerc@msn.com>
2024-07-01 17:16:39 -0700

    Delete PyTools4Windows directory

--- PyTools4Windows/open_windows.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start explorer.exe "C:\Users\litme\PyTools4Windows"
-start explorer.exe "C:\Users\litme\Desktop"
--- PyTools4Windows/remove_reg_keys.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v PyTools4Windows_open_windows /f
-reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v PyTools4Windows_remove_reg_keys /f
