57af8c735089f619fa49782534414285db1da083
Casey Litmer <litmerc@msn.com>
2026-02-26 05:23:39 -0800

    [Fix] empty_message no longer requires quotes in Colors

--- menucmd/dsl/lines_to_dict.py
+++ menucmd/dsl/lines_to_dict.py
@@ -131,9 +131,9 @@ def check_quotes(key: str, val: str, block_name: str):
     """Enforce quotes for string values."""
 
     # Keys that are expected to be quoted
-    quoted_keys = { 'exit_key', 'exit_message', 'empty_message'} \
-    | ({ 'key', 'message' } if block_name in { "Item" } else set()) \
-    | ({ 'name' } if block_name in { "Menu" } else set())
+    quoted_keys = { 'exit_key', 'exit_message' } \
+    | ({ 'key', 'message', 'empty_message' } if block_name in { "Item" } else set()) \
+    | ({ 'name', 'empty_message' } if block_name in { "Menu" } else set())
 
     if key in quoted_keys:
         if not ((val.startswith('"') and val.endswith('"')) or
