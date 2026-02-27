c23f3f33de4f6dafe5bc46b7af04fccb5f8c81ff
Casey Litmer <litmerc@msn.com>
2026-02-26 23:54:31 -0800

    [Rename] to_self -> open_self

--- menucmd/builtins/returns.py
+++ menucmd/builtins/returns.py
@@ -6,8 +6,9 @@ def open_menu(menu: str, arg = None):
     """Open menu in func chain"""
     return Return(val = arg, code = "MENU", menu = menu)
 
+def open_self(arg = None): 
+    return Return(val = arg, code="CONT")
+
 def to_menu(menu: str):
     """Open menu in Menu.*_to functions"""
     return lambda arg: Return(val = arg, code = "MENU", menu = menu)
-
-to_self = lambda arg: Return(arg, code="CONT")
